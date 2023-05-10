407828090c79edab7527e48bd50b4ad787693a31
Mmila <hpanan@connect.ust.hk>
2023-05-11 00:24:44 +0800

    update getalllist

--- faas/getalllist/handler.py
+++ faas/getalllist/handler.py
@@ -1,15 +1,14 @@
+from pymongo import MongoClient
 import json
 import os
 import sys
-
-from pymongo import MongoClient
 
 script_dir = os.path.dirname(__file__)
 mymodule_dir = os.path.join(script_dir, 'common')
 sys.path.append(mymodule_dir)
 
 from shared import (
-    handle_decimal_type
+    NotFoundException,
     get_user_id,
     get_headers,
 )
@@ -42,12 +41,12 @@ def handle(event, context):
         q = {'pk':key_string}
         item_list = list(mycol.find(q))
    # all_product = list(mycol.find())   
-    for item in product_list:
+    for item in item_list:
         item.update(
             (k, v.replace("item#", "")) for k, v in item.items() if k == "sk"
         )
     return {
         "statusCode": 200,
         "headers": get_headers(user_id),
-        "body": json.dumps({"products": item_list,}, default = str)
+        "body": json.dumps({"items": item_list, "message": "show all item"}, default = str)
     }
