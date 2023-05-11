41ce4e1df76a441d83749dc32d65ef58f57003cb
cccccci <709313510@qq.com>
2023-05-12 02:15:07 +0800

    change api url

--- www/src/config.js
+++ www/src/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-  "api_base_url": "http://gateway.openfaas.svc.cluster.local:8080/function",                                     // TodoFunctionApi
+  "api_base_url": "http://127.0.0.1:8080/function",                                     // TodoFunctionApi
   "redirect_url": "http://frontend-service"                                      // AmplifyURL
 };
 
