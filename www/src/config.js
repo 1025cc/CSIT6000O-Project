8478c6c6f9430ccefa01a782a3bdf7aff33cc564
cccccci <709313510@qq.com>
2023-05-12 17:33:20 +0800

    fix App.js

--- www/src/config.js
+++
@@ -1,7 +0,0 @@
-
-const config = {
-  "api_base_url": "http://127.0.0.1:8080/function",                                     // TodoFunctionApi
-  "redirect_url": "http://frontend-service"                                      // AmplifyURL
-};
-
-export default config;
