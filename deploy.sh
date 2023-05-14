86cfd03052075094ff2d05d91d4ed1a31e2adee4
Mmila <hpanan@connect.ust.hk>
2023-05-14 19:34:59 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -48,7 +48,7 @@ echo "> Port forwarding for mongodb-service"
 kubectl port-forward -n openfaas-fn svc/mongodb-service 27017:27017 --address=0.0.0.0 &
 
 echo "> Waiting until MongoDB to launch on 27017"
-while ! nc -z localhost 27017; do
+while kubectl get pods -n openfaas-fn | grep mongodb-deployment | grep Running; do
 	sleep 1
 done
 
